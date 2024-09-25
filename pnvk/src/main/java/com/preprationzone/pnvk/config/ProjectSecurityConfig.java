d70a2b4fb7efbb529e6ea480219a95f23b50e8e4
kishorevbhosale <kishorevbhosale@gmail.com>
2024-09-26 03:21:45 +0530

    Custom Security Configurations for public pages & CSRF Disable

--- pnvk/src/main/java/com/preprationzone/pnvk/config/ProjectSecurityConfig.java
+++ pnvk/src/main/java/com/preprationzone/pnvk/config/ProjectSecurityConfig.java
@@ -11,7 +11,14 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests( request -> request.anyRequest().permitAll())
+        http.csrf((csrf)-> csrf.disable())
+                .authorizeHttpRequests((request) -> request.requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/holidays/**").permitAll()
+                        .requestMatchers("/contact").permitAll()
+                        .requestMatchers("/saveMsg").permitAll()
+                        .requestMatchers("/courses").permitAll()
+                        .requestMatchers("/about").permitAll()
+                        .requestMatchers("/assets/**").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
