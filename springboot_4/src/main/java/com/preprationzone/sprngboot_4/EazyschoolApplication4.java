3a086b9ce8325b90d2ef6d43a59453401335cd69
kishorevbhosale <kishorevbhosale@gmail.com>
2024-09-26 08:04:14 +0530

    Implement Error page using ControllerAdvice annotation

---
+++ springboot_4/src/main/java/com/preprationzone/sprngboot_4/EazyschoolApplication4.java
@@ -0,0 +1,13 @@
+package com.preprationzone.sprngboot_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyschoolApplication4 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EazyschoolApplication4.class, args);
+    }
+
+}
---
+++ springboot_4/src/test/java/com/preprationzone/sprngboot_4/Sprngboot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preprationzone.sprngboot_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprngboot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
